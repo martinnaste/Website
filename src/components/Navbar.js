bb9be75455fb205b77422953035f64ff10bc50f2
martinnaste <martin.naste97@hotmail.com>
2022-02-14 14:36:19 +0800

    link9

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -30,7 +30,7 @@ function Navbar() {
         <>
             <nav className='navbar' id='hero'>
                 <div className='navbar-container'>
-                    <a href={useLocation().pathname !== '/website/' ? 'website' : '/'} onClick={closeMobileMenu}>
+                    <a href='/' onClick={closeMobileMenu}>
                         {console.log("UL: ", useLocation().pathname)}
                         <img height="120px" src={require("../assets/images/MNpx2.png")} className='img1' alt='MN'/>
                     </a>
