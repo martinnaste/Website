c32660c9a21b4cbbba2c080c87afcc32f6096a12
martinnaste <martin.naste97@hotmail.com>
2022-02-15 09:34:35 +0800

    Links13

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -12,7 +12,7 @@ function CardItem(props) {
         if (props.path.includes('http')){
             console.log("http ", props.path)
             setLink(1);
-        } else if(props.path === 'website/') {
+        } else if(props.path === '') {
             console.log("website/ ", props.path)
             setLink(2);
         } else {
