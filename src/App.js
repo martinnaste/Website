c084d3f2b9d873f77afedad79614dd1916dbf209
martinnaste <martin.naste97@hotmail.com>
2023-09-13 16:23:54 +0800

    Test navigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Home from './components/Pages/Home';
-import Footer from './components/Footer';
-import { ToastContainer } from 'react-toastify';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import "./App.css";
+import Home from "./components/Pages/Home";
+import Footer from "./components/Footer";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Capstone from './components/Pages/Capstone';
-import ScrollToTop from './components/ScrollToTop';
+import Capstone from "./components/Pages/Capstone";
+import ScrollToTop from "./components/ScrollToTop";
 
 function App() {
+  const globalNavigateToCapstone = () => {
+    Navigate("/capstone");
+  };
+
+  window.GLOBALNAVTOCAPSTONE = globalNavigateToCapstone;
+
   return (
     <div className="App">
       <Router>
         <ScrollToTop />
         <Navbar />
         <Routes>
-          <Route path='/' exact element={<Home/>} />
-          <Route path='/capstone' element={<Capstone />} />
+          <Route path="/" exact element={<Home />} />
+          <Route path="/capstone" element={<Capstone />} />
         </Routes>
         <ToastContainer limit={6} />
         <Footer />
