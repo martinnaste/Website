151050e5218c8d3889faf4e0b073959f45516ac7
martinnaste <martin.naste97@hotmail.com>
2023-09-13 16:49:32 +0800

    Remove navigation etc

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -3,16 +3,8 @@ import "../../App.css";
 import About from "../About";
 import Cards from "../Cards";
 import HeroSection from "../HeroSection";
-import { useNavigate } from "react-router-dom";
 
 function Home() {
-  const navigate = useNavigate();
-  const globalNavigateToCapstone = () => {
-    navigate("/capstone");
-  };
-
-  window.GLOBALNAVTOCAPSTONE = globalNavigateToCapstone;
-  console.log(window.location.href);
   return (
     <>
       <HeroSection />
